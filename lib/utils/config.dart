dcb30385238d1c5692e6f7f773ba1af1537ff13d
ShyYigII <DungDT.B21CN263@stu.ptit.edu.vn>
2024-05-01 12:00:00 +0700

    Fix config

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const String apiUrl = "https://api.example.com";
+  static const String apiUrl = "https://172.0.0.1";
   static const String appName = "My Awesome App";
   static const bool isDebugMode = true;
 

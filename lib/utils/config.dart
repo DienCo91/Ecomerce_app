207f27b6149c755bd77d83bf0b8662e33dadf386
ShyYigII <DungDT.B21CN263@stu.ptit.edu.vn>
2025-04-29 14:45:20 +0700

    Your commit message

---
+++ lib/utils/config.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  static const String apiUrl = "https://api.example.com";
+  static const String appName = "My Awesome App";
+  static const bool isDebugMode = true;
+
+  // Các thông số khác tùy dự án
+}

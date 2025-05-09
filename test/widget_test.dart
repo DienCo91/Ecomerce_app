070942f99575eda7d49c9b1bcbd78385397beb90
nhynhy20 <nhivu208@gmail.com>
2025-05-09 14:19:21 +0700

    add/: test ui products

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,80 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// // This is a basic Flutter widget test.
+// //
+// // To perform an interaction with a widget in your test, use the WidgetTester
+// // utility in the flutter_test package. For example, you can send tap and scroll
+// // gestures. You can also use WidgetTester to find child widgets in the widget
+// // tree, read text, and verify that the values of widget properties are correct.
+
+// import 'package:flutter/material.dart';
+// // import 'package:flutter_test/flutter_test.dart';
+
+// // import 'package:flutter_app/main.dart';
+
+// // void main() {
+// //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+// //     // Build our app and trigger a frame.
+// //     await tester.pumpWidget(const MyApp());
+
+// //     // Verify that our counter starts at 0.
+// //     expect(find.text('0'), findsOneWidget);
+// //     expect(find.text('1'), findsNothing);
+
+// //     // Tap the '+' icon and trigger a frame.
+// //     await tester.tap(find.byIcon(Icons.add));
+// //     await tester.pump();
+
+//     // Verify that our counter has incremented.
+//     expect(find.text('0'), findsNothing);
+//     expect(find.text('1'), findsOneWidget);
+//   });
+// }
+
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_app/models/products.dart';
+import 'package:flutter_app/widgets/header.dart';
 
-import 'package:flutter_app/main.dart';
+class ProductsManage extends StatefulWidget {
+  const ProductsManage({super.key});
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  @override
+  State<ProductsManage> createState() => _ProductsManageState();
+}
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+class _ProductsManageState extends State<ProductsManage> {
+  List<Products> products = [];
+  bool _isLoading = false;
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  void getData() async {}
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(toolbarHeight: 0),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Header(icon: Icons.list, title: "Products", iconColor: Colors.blue),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: products.length,
+            padding: const EdgeInsets.only(bottom: 100),
+            itemBuilder: (context, index) {
+              Products product = products[index];
+
+              return Container(child: Text(product.name));
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
